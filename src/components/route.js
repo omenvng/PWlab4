67891b29675d6f22ec1854f27d7e330276604ad4
oMen <oMen>
2022-05-14 20:35:54 +0300

    added report

--- src/components/route.js
+++ src/components/route.js
@@ -14,11 +14,7 @@ export default function MainComponent() {
       return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/main" element={<UserCheck Comp={Main} />} />
-        <Route path="/login" element={<UserCheck Comp={Login} />} />
-        <Route exact path="/quizzes" element={<UserCheck Comp={Quizes} />} />
-        <Route path="/quizzes/:quizId" element={<UserCheck Comp={Quiz  }  />} />
+      <Routes><Route path="/main" element={<UserCheck Comp={Main} />} /><Route path="/login" element={<UserCheck Comp={Login} />} /><Route exact path="/quizzes" element={<UserCheck Comp={Quizes} />} /><Route path="/quizzes/:quizId" element={<UserCheck Comp={Quiz  }  />} />
       </Routes>
     </div>
   );
